7d55e77af56646a7a83873aa3569b75eae95c6cc
tanmald <23216321+tanmald@users.noreply.github.com>
2017-03-25 18:11:37 +0000

    updated class TwitterClient

--- src/com/socialgeomovie/clients/TwitterClient.java
+++ src/com/socialgeomovie/clients/TwitterClient.java
@@ -1,15 +1,10 @@
 package com.socialgeomovie.clients;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
@@ -28,17 +23,33 @@ public class TwitterClient {
 	private static final String ACCESS_TOKEN = "841224583051149313-vvUu5cxygGm9t1uZg0QPSgdgs9BTkww";
 	private static final String ACCESS_TOKEN_SECRET = "fHj97HjevuabndTH62GIKBfIaCCafENyKsPOXUSxTiJVg";
 	
-	public Object getTweet(){
+	public ArrayList<String> getTweet(int nrTweets) throws InterruptedException{
 		
-		Authentication hosebirdAuth = new OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
-		
-		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+		// Create an appropriately sized blocking queue
+		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(nrTweets);
 		BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
+		
+		// Authenticate via OAuth
+		Authentication hosebirdAuth = new OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
 		// Declare the host we want to connect to, the endpoint, and authentication
 		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
 		StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+
+		// Build a hosebird client
+		ClientBuilder builder = new ClientBuilder()
+				.hosts(hosebirdHosts)						// add Constants.STREAM_HOST
+				.authentication(hosebirdAuth)
+				.endpoint(hosebirdEndpoint)
+				.processor(new StringDelimitedProcessor(msgQueue))
+				.eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
 		
+		// Creates a client
+		Client hosebirdClient = builder.build();
+		
+		// Attempts to establish a connection
+		hosebirdClient.connect();
+				
 		// Followings and track terms
 		List<Long> followings = new ArrayList<Long>();
 		followings.add(1234L);
@@ -53,39 +64,17 @@ public class TwitterClient {
 		hosebirdEndpoint.trackTerms(terms);
 		hosebirdEndpoint.languages(languages);
 		
-		ClientBuilder builder = new ClientBuilder()
-				  .name("Hosebird-Client-01")                              // optional: mainly for the logs
-				  .hosts(hosebirdHosts)
-				  .authentication(hosebirdAuth)
-				  .endpoint(hosebirdEndpoint)
-				  .processor(new StringDelimitedProcessor(msgQueue))
-				  .eventMessageQueue(eventQueue);                          // optional: use this if you want to process client events
-
-				Client hosebirdClient = builder.build();
-				// Attempts to establish a connection.
-				hosebirdClient.connect();
-				
-		// on a different thread, or multiple different threads....
-		Gson gson = new Gson();
-		while (!hosebirdClient.isDone())
-		{
-			try
-			{
-				String msg = msgQueue.take();
-				Type token = new TypeToken<HashMap<String,Object>>(){}.getType();
-				Map<String,Object> tweet = gson.fromJson(msg, token);
-				return(tweet.get("text"));
-			} 
-			catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-				//something(msg);
-				//profit();
-			}		
+		hosebirdClient.connect();
+		
+		// Creates a list to save the iterated tweets
+		ArrayList<String> tweetsList = new ArrayList<String>();
+		while (!hosebirdClient.isDone()) {
+			String message = msgQueue.take();
+			tweetsList.add(message); 
+		} 	
 				
 		hosebirdClient.stop();
-	}
-
+		return tweetsList;
+  }
+	
 }
