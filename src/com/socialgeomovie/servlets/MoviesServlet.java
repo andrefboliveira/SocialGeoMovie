2d02aeb678f2e4068f88147affbf1c9c9a1297d6
alexanderjpf <alexanderjpf@6603e5e7-34c0-41aa-a568-227eef8ba682>
2017-03-26 22:15:24 +0000

    -modified the import process to as to store movie and cast data

--- src/com/socialgeomovie/servlets/MoviesServlet.java
+++ src/com/socialgeomovie/servlets/MoviesServlet.java
@@ -1,7 +1,11 @@
 package com.socialgeomovie.servlets;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,14 +16,18 @@ import javax.ws.rs.core.Response.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+import com.socialgeomovie.clients.Neo4JClient;
 import com.socialgeomovie.clients.OpenSubsClient;
 import com.socialgeomovie.clients.TraktClient;
 import com.socialgeomovie.clients.TwitterClient;
 import com.socialgeomovie.pojos.Subtitle;
+import com.socialgeomovie.utils.Converter;
 import com.uwetrottmann.trakt5.entities.CastMember;
 import com.uwetrottmann.trakt5.entities.Movie;
 
 // http://localhost:8080/aw2017/rest/movies
+
 @Path("movies")
 public class MoviesServlet 
 {
@@ -42,6 +50,9 @@ public class MoviesServlet
 		TraktClient trakt = new TraktClient();
 		OpenSubsClient openSubs = new OpenSubsClient();
 		TwitterClient twitterClient = new TwitterClient();
+		//Neo4JClient.setUniquenessConstraint("Movie", "imdb");
+		
+		Map<String, Integer> report = new HashMap<String, Integer>();
 		
 		List<Movie> movies;
 		try 
@@ -49,43 +60,68 @@ public class MoviesServlet
 			movies = trakt.getPopularMovies(1, 10);
 			
 			int count = movies.size();
+			report.put("movies", count);
+			report.put("cast members", 0);
+			report.put("subtitles", 0);
+			report.put("tweets", 0);
 			for(int i=0; i<count; i++)
 			{
 				Movie movie = movies.get(i);
 				
-				logger.info(movie.title);
-				 
+				logger.info("Processing movie: " + movie.title);
 				// TODO store movie data
+				//Neo4JClient.createNodeWithProperties("Movie", m);
+				URI movieNode = Neo4JClient.createNodeWithProperties("Movie", Converter.movie2Map(movie));
+				
+				
 				
 				//System.out.println(movie.ids.imdb);
 				List<CastMember> cast = trakt.getCast(movie.ids.trakt+"");
 				int castCount = cast.size();
+				report.put("cast members", castCount + report.get("cast members"));
 				for(int j=0; j<castCount; j++)
 				{
 					CastMember castMember = cast.get(j);
-					logger.info(castMember.person.name);
+					logger.info("adding cast :" +castMember.person.name);
 					// TODO store cast information
+					
+					Map<String, Object> castData = Converter.cast2Map(castMember);
+					String character = (String) castData.get("character");
+					castData.remove("character");
+					URI castNode = Neo4JClient.createNodeWithProperties("Cast", castData);
+					Map<String, String> characterMap = new HashMap<String, String>();
+					characterMap.put("character", character);
+					URI relationship = Neo4JClient.createRelationshipWithProperties(castNode, movieNode, "acts in", characterMap);
 				}
 
 				// TODO get subtitles
 				Subtitle subtitle = openSubs.getSubtitle(movie.ids.imdb);
 				if(subtitle != null)
 				{
-				
-//					logger.info(subtitle.toString());
+					report.put("subtitles", 1 + report.get("subtitles"));
+					logger.info("Downloded movie substitle");
+					//logger.info(subtitle.toString());
 					// TODO process subtitles
 					// TODO store subtitle information
 				}
 				
-				twitterClient.getTweet(10);
+				int tweetCount = 10;
+				twitterClient.getTweet(tweetCount);
+				report.put("tweets", tweetCount + report.get("tweets"));
+				
+				logger.info("Import process done");
 			}
 		}
-		catch (IOException | InterruptedException e)
+		catch (IOException | InterruptedException | URISyntaxException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return Response.status(Status.OK).entity("{\"status\":\"OK\"}").build();
+		Gson gson = new Gson();
+		logger.info("Movie data imported");
+		return Response.status(Status.OK).entity(gson.toJson(report)).build();
 	}
+	
+	
 }
---
+++ src/com/socialgeomovie/utils/Converter.java
@@ -0,0 +1,42 @@
+package com.socialgeomovie.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.uwetrottmann.trakt5.entities.CastMember;
+import com.uwetrottmann.trakt5.entities.Movie;
+
+public class Converter 
+{
+	public static Map<String, Object> movie2Map(Movie movie)
+	{
+		Map<String, Object> movieData = new HashMap<String, Object>();
+		movieData.put("id_imdb", movie.ids.imdb);
+		movieData.put("id_trakt", movie.ids.trakt);
+		movieData.put("id_tmdb", movie.ids.tmdb);
+		movieData.put("rating", movie.rating);
+		movieData.put("title", movie.title);
+		movieData.put("tagline", movie.tagline);
+		movieData.put("certification", movie.certification);
+		movieData.put("homepage", movie.homepage);
+		movieData.put("trailer", movie.trailer);
+		movieData.put("released", movie.released.toString());
+		movieData.put("runtime", movie.runtime);
+		movieData.put("overview", movie.overview);
+		//movieData.put(MovieProperties.GENRES.toString(), movie.genres);
+	
+		return movieData;
+	}
+	
+	public static Map<String, Object> cast2Map(CastMember cast)
+	{
+		Map<String, Object> castData = new HashMap<String, Object>();
+		castData.put("character", cast.character);
+		castData.put("name",cast.person.name);
+		castData.put("id_trakt",cast.person.ids.trakt);
+		castData.put("id_imdb",cast.person.ids.imdb);
+		castData.put("id_tmdb",cast.person.ids.tmdb);
+		
+		return castData;
+	}
+}
