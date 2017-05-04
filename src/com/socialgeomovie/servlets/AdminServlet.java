a2409ce5c7f373009858e2204d093fb7e41d692f
tanmald <23216321+tanmald@users.noreply.github.com>
2017-05-04 22:04:13 +0000

    Added Tweet response

--- src/com/socialgeomovie/servlets/AdminServlet.java
+++ src/com/socialgeomovie/servlets/AdminServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
@@ -11,6 +12,7 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -19,6 +21,7 @@ import javax.ws.rs.core.Response.Status;
 
 import com.google.gson.Gson;
 import com.socialgeomovie.clients.SaveDataClient;
+import com.socialgeomovie.clients.TwitterClient;
 import com.socialgeomovie.servlets.MovieServlet.MoviePeople;
 
 @Path("/db")
@@ -127,4 +130,23 @@ public class AdminServlet {
 		}
 	}
 
+	@GET
+    @Path("/tweets")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response importTweets(@PathParam("movie_id") int movie_id) 
+    {
+//        TwitterClient twitterClient = new TwitterClient();
+
+        // TODO needs to fetch the movie title from the database
+        try {
+            List<String> tweets = TwitterClient.getTweets("movie.title");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Gson gson = new Gson();
+        return Response.status(Status.OK).entity("gson.toJson(report)").build();
+    }
 }
